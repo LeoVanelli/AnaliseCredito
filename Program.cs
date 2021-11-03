a761e24720c5cf919cb995f7b02ccac8bb14bb96
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-11-03 19:51:19 -0300

    Adiciona atividade AnaliseCredito

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AnaliseCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------- Analise de Crédito ----------");
+
+            Console.Write("Digite o valor do empréstimo desejado(R$): ");
+            double.TryParse(Console
+            .ReadLine()
+            .ToString(), out double _emprestimo);
+            Console.Write("Digite a quantidade de parcelas..........: ");
+            double.TryParse(Console
+            .ReadLine()
+            .ToString(), out double _parcelas);
+            Console.Write("Digite sua renda mensal(R$)..............: ");
+            double.TryParse(Console
+            .ReadLine()
+            .ToString(), out double _rendaMensal);
+
+            Console.Write("\nCalculando crédito");
+            System.Threading.Thread.Sleep(1000);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(". ");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            string _resultado = (_emprestimo/_parcelas) < ((_rendaMensal*30)/100) ? $"Crédito Aprovado.\nValor da parcela: {_parcelas}x de {_emprestimo/_parcelas}" : "Credito Reprovado, o valor da parcela solicitada ultrapassou 30% da sua renda.";
+
+            Console.Write($"\n\nResultado: {_resultado}");
+        }
+    }
+}
